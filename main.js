ad8eed80449cae155b08053cfbe3db0f54cbe42b
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2023-01-31 13:55:07 -0300

    day 3

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+//Day 1
+/*
 let numeroUm = 1
 let stringUm = '1'
 let numeroTrinta = 30
@@ -22,19 +25,67 @@ if (numeroDez == stringDez) {
 } else {
   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
 }
+*/
 
-
-nome = prompt("Qual o seu nome?");
-idade = prompt("Quantos anos você tem?");
-linguagem = prompt("Qual linguagem de programação você está estudando?");
+// Day 2
+/* 
+let nome = prompt("Qual o seu nome?");
+let idade = prompt("Quantos anos você tem?");
+let linguagem = prompt("Qual linguagem de programação você está estudando?");
 
 alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
 
-resposta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`)
+let resposta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`)
 
 if (resposta == 1){
     alert("Muito bom! Continue estudando e você terá muito sucesso.")
 }
 if (resposta == 2){
     alert("Ahh que pena... Já tentou aprender outras linguagens?")
+}
+*/
+
+// Day 3
+
+
+let resposta1 = prompt("Gostaria de seguir para área de Front-End ou seguir para a área de Back-End? (1 para front, 2 para back)");
+let resposta2 = ""
+if (resposta1 == 1) {
+    resposta1 = "Front-End";
+    
+    resposta2 = prompt("Gostaria de aprender React ou Vue? (1 para react, 2 para Vue)");
+    
+    if(resposta2 == 1){
+        resposta2 = "React";
+    } else if(resposta2 == 2){
+        resposta2 = "Vue";
+    } else{
+        alert("Você não digitou nenhuma resposta válida");
+    }
+} else if (resposta1 == 2) {
+    resposta1 = "Back-End";
+    
+    resposta2 = prompt("Gostaria de aprender C# ou Java? (1 para C#, 2 para Java)");
+    if(resposta2 == 1){
+        resposta2 = "C#";
+    } else if(resposta2 == 2){
+        resposta2 = "Java";
+    } else{
+        alert("Você não digitou nenhuma resposta válida");
+    }
+} else{
+    alert("Você não digitou nenhuma resposta válida");
+}
+
+let resposta3 = prompt(`Gostaria de se especializar em ${resposta1} e se aprofundar mais em ${resposta2} ou gostaria de se tornar FullStack? (1 para se especializar, 2 para FullStack)` )
+
+if (resposta3 != "2" || resposta3 != "1"){
+    alert("Você não digitou nenhuma resposta válida");
+}  
+
+let resposta4 = prompt("Tem mais alguma tecnologia que gostaria de se especializar?(ok para sim)");
+let resposta5 = "";
+while(resposta4 == "ok"){
+    resposta5 = prompt("Quais tecnologias gostaria de se especializar?");
+    resposta4 = prompt("Tem mais alguma tecnologia que gostaria de se especializar?(ok para sim)");
 }
